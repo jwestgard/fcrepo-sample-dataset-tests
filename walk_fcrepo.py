494b02e6e1088f8171b3898aac6057a8728e0167
Joshua Westgard <jwestgard@mac.com>
2016-09-26 11:02:16 -0400

    remove comments mistakenly copied from elsewhere

--- walk_fcrepo.py
+++ walk_fcrepo.py
@@ -54,18 +54,18 @@ def main():
     parser = argparse.ArgumentParser(
         description='Walking a Fedora repository.'
         )
-    # Path to the repo config (endpoint, credentials, and WebAC paths)
+    # Repository credentials
     parser.add_argument('-u', '--user',
                         help='Repository credentials in the form username:password.',
                         action='store',
                         required=False
                         )
-    # Path to the data set (metadata and files)
+    # Path to the root node to walk
     parser.add_argument('root',
                         help='Path to root node from which to begin walk.',
                         action='store'
                         )
-    # Path to the data set (metadata and files)
+    # File in which to store the URIs found
     parser.add_argument('output',
                         help='Path to file to store output.',
                         action='store'
