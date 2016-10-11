938fe10f2005ede116647cab731526e2c8470f1e
jwestgard <jwestgard@mac.com>
2016-10-10 22:30:38 -0400

    fix fcrepo walk to capture fcr:metadata nodes

--- diff-fcrepo.py
+++ diff-fcrepo.py
@@ -37,7 +37,8 @@ def is_binary(node, auth):
 def get_child_nodes(node, auth):
     ''' Get the children based on LDP containment. '''
     if is_binary(node, auth):
-        return None
+        metadata = [node + "/fcr:metadata"]
+        return metadata
     else:
         response = requests.get(node, auth=auth)
         if response.status_code == 200:
@@ -61,43 +62,63 @@ def get_directory_contents(localpath):
         return [p.path for p in os.scandir(localpath)]
 
 
-class Resource_walker:
-    ''' Walk an fcrepo repository either online (in LDP) or serialized. '''
-
-    def __init__(self, root, auth):
+class Walker:
+    ''' Walk a set of Fedora resources. '''
+    def __init__(self, root):
         self.to_check = [root]
-        self.auth = auth
       
     def __iter__(self):
         return self
+
+
+class Fcrepo_walker(Walker):
+    ''' Walk resources in a live repository. '''
+    def __init__(self, root, auth):
+        Walker.__init__(self, root)
+        self.auth = auth
+    
+    def __next__(self):
+        if not self.to_check:
+            raise StopIteration()
+        else:
+            current = self.to_check.pop()
+            children = get_child_nodes(current, self.auth)
+            if children:
+                self.to_check.extend(children)
+            return current
+
+
+class Local_walker(Walker):
+    ''' Walk serialized resources on disk. '''
+    def __init__(self, root):
+        Walker.__init__(self, root)
         
     def __next__(self):
         if not self.to_check:
             raise StopIteration()
         else:
             current = self.to_check.pop()
-            if current.startswith('http'):
-                children = get_child_nodes(current, self.auth)
-            else:
-                children = get_directory_contents(current)
+            children = get_directory_contents(current)
             if children:
                 self.to_check.extend(children)
-            return current
+                return None
+            else:
+                return current
 
 
 class Resource():
     ''' Object representing a resource, either local or in fcrepo. '''
-    def __init__(self, root, path):
-        self.path = path
-        self.relpath = os.path.relpath(path, root)
-        
+    def __init__(self, current, other, path):
+        self.fullpath = path
+        self.relpath = os.path.relpath(self.fullpath, current)
+        self.counterpart = os.path.join(other, self.relpath)
 
 
 def main():
 
-    parser = argparse.ArgumentParser(description='''Walk an LDP repository, 
-                                                    writing resource URIs to 
-                                                    list.'''
+    parser = argparse.ArgumentParser(description='''Compare two sets of Fedora 
+                                                    resources, either online or
+                                                    serialized on disk.'''
                                                     )
         
     # Repository credentials
@@ -117,12 +138,8 @@ def main():
                         )
                         
     # Paths to the two root dirs to walk
-    parser.add_argument('root1',
-                        help='Path to first root from which to begin walk.',
-                        action='store'
-                        )
-    parser.add_argument('root2',
-                        help='Path to second root from which to begin walk.',
+    parser.add_argument('repos', nargs='+',
+                        help='Paths to repo roots from which to begin walk.',
                         action='store'
                         )
                         
@@ -142,43 +159,42 @@ def main():
     else:
         fh = sys.stdout
 
-    
+
     # loop over each resource set
-    for root in (args.root1, args.root2):
+    for x in range(len(args.repos)):
+        current = args.repos[x]
+        other = args.repos[(x + 1) % len(args.repos)]
         print()
-        print('checking {0}'.format(root))
+        print('checking {0}'.format(current))
         counter = 0
         
         # for each resource in the tree
-        for path in Resource_walker(root, auth):
-            r = Resource(root, path)
-            print(r.path, r.relpath)
+        if current.startswith('http://'):
+            resource_set = Fcrepo_walker(current, auth)
+        else:
+            resource_set = Local_walker(current)
             
-            fh.write(path + '\n')
-            counter += 1
+        for path in resource_set:
+            if path is None:
+                continue
+            else:
+                r = Resource(current, other, path)
+                print(r.fullpath, r.counterpart)
             
-            if args.output:
-                print("resources checked: {0}".format(counter), end='\r')
+                fh.write(path + '\n')
+                counter += 1
+            
+                if args.output:
+                    print("resources checked: {0}".format(counter), end='\r')
         
         # line feed to preserve counter 
         print()
 
-
-
-  
     # loop over one set
     # look for resource in other set
     # if a binary, compare checksums
     # if rdf, compare sets of triples
     # loop over other set and perform all checks
-    
-    
-    
-    
-        
-        
-        
-    
     
     # close file if one was opened
     if args.output:
