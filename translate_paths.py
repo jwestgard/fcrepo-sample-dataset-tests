262df44828625da5a6d1a2a3cdd4e43c31164a99
jwestgard <jwestgard@mac.com>
2016-10-13 00:09:48 -0400

    typing resources based on uri and config

--- translate_paths.py
+++ translate_paths.py
@@ -1,10 +1,26 @@
 #!/usr/bin/env python3
 
 import os
+import requests
 import sys
 
 PATH = sys.argv[1]
 CONFIGFILE = sys.argv[2]
+auth = tuple(sys.argv[3].split(':'))
+
+
+def is_binary(node, auth):
+    ''' Using link headers, determine whether a resource is rdf or non-rdf. '''
+    response = requests.head(url=node, auth=auth)
+    if response.status_code == 200:
+        if response.links['type']['url'] == \
+            'http://www.w3.org/ns/ldp#NonRDFSource':
+            return True
+        else:
+            return False
+    else:
+        print("Error communicating with repository.")
+        sys.exit(1)
 
 
 class Config():
@@ -26,15 +42,28 @@ class Config():
 
 
 class Resource():
-    def __init__(self, path, config):
-        self.origpath = path
+    def __init__(self, resourcepath, config):
+        self.origpath = resourcepath
         
-        if path.startswith('http://'):
-            self.type = 'remote'
+        if self.origpath.startswith('http://'):
+            self.location = 'remote'
+            if is_binary(self.origpath, auth):
+                self.type = 'binary'
+            else:
+                self.type = 'rdf'
+            self.relpath = self.origpath[len(config.repo):]
+            
         else:
-            self.type = 'local'
+            self.location = 'local'
+            if os.path.splitext(self.origpath)[1] == '.binary':
+                self.type = 'binary'
+                self.relpath = self.origpath[len(config.bin):]
+            else:
+                self.type = 'rdf'
+                self.relpath = self.origpath[len(config.desc):]
+                
         
-        for prefix in [config.repo, config.desc, config.bin]:
+'''        for prefix in [config.repo, config.desc, config.bin]:
             if path.startswith(prefix):
                 self.relpath = path.lstrip(prefix)
                 if self.origpath.endswith(self.relpath):
@@ -46,16 +75,17 @@ class Resource():
             print('Error in path!')
         
         if self.type == 'local':
-            self.altpath = config.repo + self.relpath
+            self.altpath = config.repo + self.relpath '''
 
 
 def main():
     c = Config(CONFIGFILE)
     r = Resource(PATH, c)
-    print("orig", r.origpath)
-    print("relpath", r.relpath)
-    print("base", r.base)
-    print("alt", r.altpath)
+    
+    print(r.origpath)
+    print(r.location)
+    print(r.type)
+    print(r.relpath)
     
 
 
